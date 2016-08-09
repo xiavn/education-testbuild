805c2976815034418b91c7e0df0a7f4487d64b48
Lianne Turnbull <lianneturnbull@gmail.com>
2016-08-09 13:55:38 +0100

    Finish tabbed content section and menu

--- js/script.js
+++ js/script.js
@@ -98,23 +98,48 @@ var tabbedContent = {
 			for (var i=0; i<this.photos.length; i++) {
 				slider += this.generateSlider(this.photos[i]);
 			}
-			var html = '<div class="arrow-slider"><img class="arrow-slider" id="arrow-left" src="../img/arrow-left-slider.png"></div><div class="photo-slider"><ul>' + slider + '</ul></div><div class="arrow-slider"><img id="arrow-right" src="../img/arrow-right-slider.png"></div>';
+			var html = '<div class="l-photo-container"><div class="arrow-slider"><img id="arrow-left" src="../img/arrow-left-slider.png"></div><div class="photo-slider"><ul>' + slider + '</ul></div><div class="arrow-slider"><img id="arrow-right" src="../img/arrow-right-slider.png"></div></div>';
 			return html;
 		}
 	},
 	"newsletters": {
-		"header": "Year 3 Science Museum Adventure!",
-		"date": "12/04/2014",
-		"text": "Miss Smith and Mrs Weston's Year 3 classes had great fun at the Science Museum this week. Their trip was to support the Amazing Bodies portion of their curriculum this year, and they made lots of amazing discoveries...",
-		"photo": "../img/news-photo.jpg",
+		"items": [
+			{
+				"date": "Friday 10th January 2014",
+				"url:": ".../files/newsjan10th.pdf"
+			},
+			{
+				"date": "Friday 10th January 2014",
+				"url:": ".../files/newsjan10th.pdf"
+			},
+			{
+				"date": "Friday 10th January 2014",
+				"url:": ".../files/newsjan10th.pdf"
+			},
+			{
+				"date": "Friday 10th January 2014",
+				"url:": ".../files/newsjan10th.pdf"
+			},
+			{
+				"date": "Friday 10th January 2014",
+				"url:": ".../files/newsjan10th.pdf"
+			},
+			{
+				"date": "Friday 10th January 2014",
+				"url:": ".../files/newsjan10th.pdf"
+			}
+		],
+		"generateNews": function(item) {
+			var newsImg = '<div class="doc-icon"><img src="../img/documenticon.png" alt="Document"></div>',
+				newsItem = '<h3>' + item.date + '</h3><a href="' + item.url + '">Download</a>',
+				html = '<div class="news-item">' + newsImg + newsItem + '</div>';
+			return html;
+		},
 		"html": function() {
-			var img = '<aside class="news-photo"><img src="' + this.photo + '" alt="Photo of the Science Museum"></aside>',
-				article = '<article class="latest-news"><h2>' + 
-				this.header + 
-				'</h2><span class="date">' + this.date + '</span><p>' + 
-				this.text + 
-				'</p><a class="read-more">Read More...</a></article>',
-				html = img + article;
+			var html = "";
+			for (var i=0; i<this.items.length; i++) {
+				html += this.generateNews(this.items[i]);
+			}
 			return html;
 		}
 	}
@@ -124,12 +149,30 @@ function generateTabbedContent(tab) {
 	$("#tabbed-content-active").html(tab.html());
 }
 
+function tabbedMenuSelect(item) {
+	$("#tabbed-nav ul li").removeClass("active");
+	item.addClass("active");
+	var tab = item.attr("id");
+	generateTabbedContent(tabbedContent[tab]);
+}
+
 $(document).ready(function() {
 	generateTabbedContent(tabbedContent.latestNews);
 	$("#tabbed-nav ul li").click(function() {
-		$("#tabbed-nav ul li").removeClass("active");
-		$(this).addClass("active");
-		var tab = $(this).attr("id");
-		generateTabbedContent(tabbedContent[tab]);
+		if ($(".tabbed-content nav ul").css("flex-flow") === "column nowrap") {
+			tabbedMenuSelect($(this));
+			$(".tab-menu-item").toggleClass("menu-hide");
+			$(".menu-small").remove();
+			$(this).append('<span class="menu-small">&equiv;</span>');
+		} else {
+			tabbedMenuSelect($(this));
+		}	
+	});
+	$("#tabbed-content-active").on("click", ".arrow-slider", function() {
+		if ($(this).find("img").attr("id") === "arrow-right") {
+			$(".photo-slider ul li:first-child").insertAfter(".photo-slider ul li:last-child");
+		} else {
+			$(".photo-slider ul li:last-child").insertBefore(".photo-slider ul li:first-child");
+		}
 	});
 });
